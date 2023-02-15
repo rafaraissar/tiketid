f6b11d2138f6869a1f131cf283b4c4a8f50c26bd
rafaraissar <rafaraissaraf@gmail.com>
2023-02-16 03:57:01 +0700

    update

--- apps/web/app/(dashboard)/home/categorysearch.js
+++ apps/web/app/(dashboard)/home/categorysearch.js
@@ -19,18 +19,18 @@ export default function Categorysearch() {
         <Link href="/id" className={styles.categorysearchitem}>
           Music
         </Link>
-        <div className={styles.categorysearchitem}>Trending This Week</div>
         <div className={styles.categorysearchitem}>Workshop</div>
-        <div className={styles.categorysearchitem}>IBL</div>
         <div className={styles.categorysearchitem}>Jakarta</div>
         <div className={styles.categorysearchitem}>Bandung</div>{" "}
         <div className={styles.categorysearchitem}>Surabaya</div>{" "}
         <div className={styles.categorysearchitem}>Fun Activities</div>{" "}
         <div className={styles.categorysearchitem}>Event Murah</div>{" "}
+        <div className={styles.categorysearchitem}>Trending This Week</div>
         <div className={styles.categorysearchitem}>Bulan Ini</div>{" "}
         <div className={styles.categorysearchitem}>Hobbies</div>{" "}
         <div className={styles.categorysearchitem}>Families</div>{" "}
         <div className={styles.categorysearchitem}>Near Event</div>{" "}
+        <div className={styles.categorysearchitem}>IBL</div>
         <div className={styles.categorysearchitem}>Lifestyle</div>{" "}
         <div className={styles.categorysearchitem}>Culture</div>{" "}
         <div className={styles.categorysearchitem}>Online Event</div>{" "}
